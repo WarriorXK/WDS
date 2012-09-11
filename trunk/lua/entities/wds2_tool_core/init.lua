2e1a0089df1b61b98fc29ae2552d0d3deef438f0
kevkevm@gmail.com <kevkevm@gmail.com@7fff7156-fbbe-11de-88c4-f1fccdb10200>
2012-09-11 23:03:51 +0000

    Fixed the WDS2_EntityShouldTakeDamage hook not damaging entities when true was returned, Added WDS2.CalculateDamageMul(DamageType, ent) which calculates the multiplier for all WDS2 damage based on armor types, Added WDS2.CheckProp(ent) which checks if a prop should be alive and kills it if it doesn't, Added a core entity which devides all damage between welded entities, the core also has its own energy that drains when the core is damaged, when the energy reaches 0 the core and all welded entities will be removed,

---
+++ trunk/lua/entities/wds2_tool_core/init.lua
@@ -0,0 +1,115 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+include('shared.lua')
+
+ENT.CorePower = 100
+ENT.MaxPower = 1000
+
+function ENT:Initialize()
+	self:SetModel("models/wds/device08.mdl")
+	self:PhysicsInit(SOLID_VPHYSICS)
+	self:SetMoveType(MOVETYPE_VPHYSICS)
+	self:SetSolid(SOLID_VPHYSICS)
+	local phys = self:GetPhysicsObject()
+	if phys:IsValid() then
+		phys:Wake()
+	end
+	self.dt.NextCoreCharge = 0
+	self.Outputs = Wire_CreateOutputs(self,{"Core Power"})
+end
+
+function ENT:SpawnFunction(p,t)
+	if !t.Hit then return end
+	local e = ents.Create(self.ClassName)
+	e.WDSO = p
+	e:Spawn()
+	e:Activate()
+	e:SetPos(t.HitPos+t.HitNormal*-e:OBBMins().z)
+	return e
+end
+
+function ENT:GetAllCoreEntities()
+	local Out = {}
+	for _,v in pairs(constraint.FindConstraints( self, "Weld" )) do
+		table.insert(Out, v.Entity[1].Entity == self and v.Entity[2].Entity or v.Entity[1].Entity)
+	end
+	return Out
+end
+
+function ENT:HasEntity(ent)
+	return table.HasValue(self:GetAllCoreEntities(), ent)
+end
+
+function ENT:Think()
+	if self.dt.NextCoreCharge < CurTime() then
+		self.CorePower = math.min(self.CorePower + 1, self.MaxPower)
+	end
+	
+	Wire_TriggerOutput(self, "Core Power", self.CorePower)
+	
+	self:NextThink(CurTime())
+	return true
+end
+
+function ENT:TakeDirectCoreDamage(damage)
+	self.CorePower = self.CorePower - damage
+	
+	self.dt.NextCoreCharge = CurTime() + 2
+	
+	if self.CorePower <= 0 then
+		self:Death()
+		return
+	end
+end
+
+function ENT:Death()
+	// Add an effect to this
+	for _,v in pairs(self:GetAllCoreEntities()) do
+		v:Remove()
+	end
+	self:Remove()
+end
+
+function ENT:TakeCoreDamage(ent, damage, typ)
+
+	local AllEnts = self:GetAllCoreEntities()
+	local DamagePerEnt = damage/#AllEnts
+	
+	WDS2.Debug.Print("WDS2 Core : " .. DamagePerEnt .. " per ent")
+	
+	for _,v in pairs(AllEnts) do
+		if ValidEntity(v) then
+			if WDS2.CanDamageEntity(v) then
+			
+				local Dmg = (DamagePerEnt * WDS2.CalculateDamageMul(typ, ent))
+				v.WDS2.Health = v.WDS2.Health - Dmg
+				
+				WDS2.Debug.Print(tostring(v) .. " damage : " .. Dmg)
+				
+				WDS2.CheckProp(v)
+				
+			end
+		end
+	end
+	
+end
+
+hook.Add("WDS2_EntityShouldTakeDamage", "WDS2_EntityShouldTakeDamage_wdscore", function(ent, damage, typ)
+
+	if ValidEntity(ent) then
+
+		for _,v in pairs(ents.FindByClass("wds2_tool_core")) do
+
+			if ent == v then v:TakeDirectCoreDamage(damage, typ) return false end
+		
+			if v:HasEntity(ent) then
+
+				v:TakeCoreDamage(ent, damage, typ)
+				return false
+				
+			end
+			
+		end
+		
+	end
+end)
