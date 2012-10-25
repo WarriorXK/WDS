4878600be5380e4de485e79d964d2c740c407b9a
kevkevm@gmail.com <kevkevm@gmail.com@7fff7156-fbbe-11de-88c4-f1fccdb10200>
2012-10-25 00:57:18 +0000

    Some minor idea changes about the pulse laser,

--- trunk/lua/entities/wds2_weapon_pulselaser/init.lua
+++ trunk/lua/entities/wds2_weapon_pulselaser/init.lua
@@ -6,6 +6,7 @@ ENT.FireSound = Sound("")
 
 ENT.ShootPos = Vector(33, 0, 0)
 
+ENT.SpinPercentage = 0
 ENT.ShouldFire = false
 ENT.NextFire = 0
 
@@ -19,7 +20,7 @@ function ENT:Initialize()
 		phys:Wake()
 	end
 	self.Inputs = Wire_CreateInputs(self,{"Fire"})
-	self.Outputs = Wire_CreateOutputs(self,{"Can Fire"})
+	self.Outputs = Wire_CreateOutputs(self,{"Spin Percentage"})
 end
 
 function ENT:SpawnFunction(p,t)
@@ -34,12 +35,27 @@ end
 
 function ENT:Think()
 	if self.NextFire <= CurTime() then
+	
 		if self.ShouldFire then
-			self:FireShot()
-			self.NextFire = CurTime()+6
-			self:EmitSound(self.FireSound)
+			
+			self.SpinPercentage = math.min(self.SpinPercentage + 1, 100)
+		
+			if self.SpinPercentage > 30 then
+			
+				self:FireShot()
+				self.NextFire = CurTime() + 1.1 - (self.SpinPercentage / 100)
+				
+			end
+		else
+		
+			self.SpinPercentage = math.max(self.SpinPercentage -1, 0)
+			
 		end
+		
 	end
+	
+	Wire_TriggerOutput(self, "Spin Percentage", self.SpinPercentage)
+	
 	self:NextThink(CurTime())
 	return true
 end
@@ -48,6 +64,13 @@ function ENT:FireShot()
 	local ShootPos = self:LocalToWorld(self.ShootPos)
 	local tr = WDS2.TraceLine(ShootPos,self:GetPos()+(self:GetForward()*50000),{self},msk)
 	
+	
+	local ed = EffectData()
+		ed:SetStart(ShootPos)
+		ed:SetOrigin(tr.HitPos)
+	util.Effect("wds2_pulselaser_beam", ed)
+	
+	self:EmitSound(self.FireSound)
 end
 
 function ENT:TriggerInput(name,val)
