4a51eccb91c58d15bdf617cc2966609766b3f318
kevkevm@gmail.com <kevkevm@gmail.com@7fff7156-fbbe-11de-88c4-f1fccdb10200>
2012-11-08 10:46:38 +0000

    The shield dome is now drawn in Lua, The shield dome now changes size over time instead of instantly, The generator is now a networked var on the shielddome, Added a radius wire input to the shield generator, Added an effect for shield hit (Doesn't work), The radius on the shield generator is now a networked variable, Moved a few values on the shield generator to the shared file,

---
+++ lua/effects/wds2_shieldhit/init.lua
@@ -0,0 +1,37 @@
+
+local Mat = "wds/effects/blankparticle"
+
+function EFFECT:Init(d)
+
+	local Size = d:GetMagnitude()
+	local Vec = d:GetStart()
+	local Pos = d:GetOrigin()
+	
+	local Emitter = ParticleEmitter(Pos)
+	
+	for i=1,500 do
+		local Particle = Emitter:Add(Mat,Pos)
+		local VelValue = 0.3 * Size
+		Particle:SetVelocity(((Vec:GetNormal() * math.random(-VelValue, VelValue)) + Vector(math.Rand(-VelValue, VelValue), math.Rand(-VelValue, VelValue), math.Rand(-VelValue, VelValue))) * 100)
+		Particle:SetAirResistance(70)
+		Particle:SetDieTime(math.Rand(0.5, 6))
+		Particle:SetStartAlpha(200)
+		Particle:SetEndAlpha(20)
+		Particle:SetGravity(Vector(50 * math.Rand(-1,1), 50 * math.Rand(-1,1), 50 * math.Rand(-1,1)))
+		Particle:SetStartSize(10 * Size)
+		Particle:SetEndSize(13.5 * Size)
+		Particle:SetRoll(0)
+		Particle:SetRollDelta(0)
+		Particle:SetColor(75, math.random(80,180), 255)
+		Particle:SetCollide(true)
+		Particle:SetCollideCallback(function(part) part:SetDieTime(0) end)
+	end
+	
+end
+
+function EFFECT:Think()
+	return false
+end
+
+function EFFECT:Render() 
+end
--- lua/entities/wds2_tool_shieldgen/shared.lua
+++ lua/entities/wds2_tool_shieldgen/shared.lua
@@ -12,10 +12,20 @@ ENT.WireDebugName	= ENT.PrintName
 ENT.Spawnable		= true
 ENT.AdminSpawnable	= true
 
+ENT.DrainedThreshold = 200
+ENT.DrainPerThink = 0
+ENT.MaxEnergy = 1000
+ENT.MaxRadius = 1024
+
 function ENT:SetupDataTables()
 	self:DTVar("Int",0,"Energy")
+	self:DTVar("Int",1,"Radius")
 end
 
 function ENT:GetEnergy()
 	return self.dt.Energy
+end
+
+function ENT:GetRadius()
+	return self.dt.Radius
 end
