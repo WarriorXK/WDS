83e3b08561595188a630708a08022d0b01e30189
kevkevm@gmail.com <kevkevm@gmail.com@7fff7156-fbbe-11de-88c4-f1fccdb10200>
2012-11-15 17:17:03 +0000

    Updated the todo, Removed all references to self.Entity, Added a non-functional (for now) secondary fire to the dual laser, Added an effect for the secondary fire on the dual laser,

--- trunk/lua/effects/wds2_gatlinggun_laser/init.lua
+++ trunk/lua/effects/wds2_gatlinggun_laser/init.lua
@@ -1,13 +1,13 @@
 
 local SpriteMat = Material("wds/effects/blankparticle")
 local LaserMat = Material("cable/redlaser")
-local Col = Color(255,70,70,150)
+local Col = Color(255, 70, 70, 150)
 
 function EFFECT:Init(d)
 	self.StartPos = d:GetStart()
 	self.EndPos = d:GetOrigin()
 	self.DieTime = CurTime() + 0.3
-	self.Entity:SetRenderBoundsWS(self.StartPos, self.EndPos)
+	self:SetRenderBoundsWS(self.StartPos, self.EndPos)
 end
 
 function EFFECT:Think()
@@ -16,7 +16,7 @@ end
 
 function EFFECT:Render()
 	render.SetMaterial(SpriteMat)
-	render.DrawSprite(self.StartPos,7,7,Col)
+	render.DrawSprite(self.StartPos, 7, 7, Col)
 	render.SetMaterial(LaserMat)
-	render.DrawBeam(self.EndPos,self.StartPos,7,0,0,Col)
+	render.DrawBeam(self.EndPos, self.StartPos, 7, 0, 0, Col)
 end
